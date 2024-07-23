bb98252409d75bca323e79199d7204eb2d9a5c53
PieyIsAPie <foreynator.serverowner@gmail.com>
2024-07-22 23:27:05 -0400

    push 01

---
+++ chore.sh
@@ -0,0 +1,10 @@
+rm -rf polo
+git clone https://github.com/polaris-linux-distro/polo.git
+cd polo
+makepkg -s --noconfirm
+cp *.pkg.tar.zst ../repo
+cd ../repo
+repo-add polaris.db.tar.gz *.pkg.tar.zst
+cd ..
+git commit "chore-update"
+git push
